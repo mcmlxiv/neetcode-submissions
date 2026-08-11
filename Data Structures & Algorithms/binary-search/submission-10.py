f9616795fb5f72bda395cf60bc8be8e539db913d
John P. <58471048+mcmlxiv@users.noreply.github.com>
2026-08-10 23:59:15 -0400

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binaryS(0, len(nums) - 1, nums, target)
+
+    
+    def binaryS(self, l:int, r:int, nums: list, target:int)-> int:
+        if l > r :
+            return -1
+
+        m = l + (r - l ) // 2
+
+        if nums[m] == target:
+            return m
+        
+        if nums[m] < target:
+            return self.binaryS(m + 1, r, nums, target)
+        
+        if nums[m] > target:
+            return self.binaryS(l, m - 1, nums, target)
+        
